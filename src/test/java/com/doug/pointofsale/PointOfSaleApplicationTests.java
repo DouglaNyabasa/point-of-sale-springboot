c5b3b6c0443664d2ce91c6a9a2e5f954f8d516b3
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-01 14:29:14 +0200

    initial commit

---
+++ src/test/java/com/doug/pointofsale/PointOfSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doug.pointofsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
