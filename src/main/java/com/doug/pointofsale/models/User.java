1a832595b2969a6f2b40a788ade935ee7564bfbe
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-03 18:05:16 +0200

    user model

---
+++ src/main/java/com/doug/pointofsale/models/User.java
@@ -0,0 +1,41 @@
+package com.doug.pointofsale.models;
+
+import com.doug.pointofsale.domain.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "Email should be valid !!")
+    private String email;
+
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private LocalDate lastLoginAt;
+
+
+
+}
