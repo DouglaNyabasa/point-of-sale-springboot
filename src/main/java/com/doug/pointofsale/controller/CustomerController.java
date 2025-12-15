d2e47a7bf1bbe07cb8851dd24e142c3763616583
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-15 17:21:54 +0200

    customer controller

--- src/main/java/com/doug/pointofsale/controller/CustomerController.java
+++ src/main/java/com/doug/pointofsale/controller/CustomerController.java
@@ -1,9 +1,49 @@
 package com.doug.pointofsale.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.doug.pointofsale.models.Customer;
+import com.doug.pointofsale.payload.response.ApiResponse;
+import com.doug.pointofsale.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) throws Exception {
+        return ResponseEntity.ok(customerService.createCustomer(customer));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) throws Exception {
+        return ResponseEntity.ok(customerService.updateCustomer(id,customer));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteCustomer(@PathVariable Long id) throws Exception {
+        customerService.deleteCustomer(id);
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Customer deleted successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Customer>> getAllCustomer() throws Exception {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Customer>> searchCustomer(@RequestParam String q) throws Exception {
+        return ResponseEntity.ok(customerService.searchCustomers(q));
+    }
+
+
 }
--- src/main/java/com/doug/pointofsale/service/CustomerService.java
+++ src/main/java/com/doug/pointofsale/service/CustomerService.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public interface CustomerService {
 
-    Customer createCustomer(Customer customer);
-    Customer updateCustomer(Long id,Customer customer);
+    Customer createCustomer(Customer customer)throws Exception;
+    Customer updateCustomer(Long id,Customer customer)throws Exception;
     void deleteCustomer(Long id) throws Exception;
     Customer getCustomer(Long id) throws Exception;
     List<Customer> getAllCustomers() throws Exception;
