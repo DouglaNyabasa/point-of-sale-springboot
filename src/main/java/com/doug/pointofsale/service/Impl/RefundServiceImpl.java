7ca1142d26e37eabbad4745811fe8c07d33e964a
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-16 12:41:21 +0200

    added methods in service class added

--- src/main/java/com/doug/pointofsale/service/Impl/RefundServiceImpl.java
+++ src/main/java/com/doug/pointofsale/service/Impl/RefundServiceImpl.java
@@ -1,4 +1,50 @@
 package com.doug.pointofsale.service.Impl;
 
-public class RefundServiceImpl {
+import com.doug.pointofsale.models.Refund;
+import com.doug.pointofsale.payload.dto.RefundDTO;
+import com.doug.pointofsale.service.RefundService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class RefundServiceImpl implements RefundService {
+    @Override
+    public RefundDTO createRefund(Refund refund) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<RefundDTO> getAllRefund() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public RefundDTO getRefundByCashId(String cashId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public RefundDTO getRefundByShiftReport(Long shiftReportId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<RefundDTO> getRefundByCashierAndDateRange(String cashierId, LocalDateTime startDate, LocalDateTime endDate) throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public List<RefundDTO> getRefundByBranch(Long branchId) throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public RefundDTO getRefundById(Long refundId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteRefund(Long refundId) throws Exception {
+
+    }
 }
