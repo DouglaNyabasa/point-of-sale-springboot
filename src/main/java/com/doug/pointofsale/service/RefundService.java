2cb697e22c6aa1cb0f92acca86813dd7915ed3bc
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-16 12:34:33 +0200

    added methods in service class

--- src/main/java/com/doug/pointofsale/service/RefundService.java
+++ src/main/java/com/doug/pointofsale/service/RefundService.java
@@ -1,4 +1,18 @@
 package com.doug.pointofsale.service;
 
+import com.doug.pointofsale.models.Refund;
+import com.doug.pointofsale.payload.dto.RefundDTO;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
 public interface RefundService {
+
+    RefundDTO createRefund(Refund refund) throws Exception;
+    List<RefundDTO> getAllRefund() throws Exception;
+    RefundDTO getRefundByCashId(String cashId) throws Exception;
+    RefundDTO getRefundByShiftReport(Long shiftReportId) throws Exception;
+    List<RefundDTO> getRefundByCashierAndDateRange(String cashierId, LocalDateTime startDate, LocalDateTime endDate) throws Exception;
+    List<RefundDTO> getRefundByBranch(Long branchId) throws Exception;
 }
