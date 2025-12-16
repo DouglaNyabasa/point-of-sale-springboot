4c458639aca36c970ecfe1f2cceb7f9641c7c9a2
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-16 10:08:36 +0200

    implemented createOrder getOrderbyId and getByBranch in controller

--- src/main/java/com/doug/pointofsale/controller/OrderController.java
+++ src/main/java/com/doug/pointofsale/controller/OrderController.java
@@ -1,4 +1,40 @@
 package com.doug.pointofsale.controller;
 
+import com.doug.pointofsale.domain.OrderStatus;
+import com.doug.pointofsale.domain.PaymentType;
+import com.doug.pointofsale.payload.dto.OrderDto;
+import com.doug.pointofsale.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto order) throws Exception {
+        return  ResponseEntity.ok(orderService.createOrder(order));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDto> getOrderById(@PathVariable Long id) throws Exception {
+        return  ResponseEntity.ok(orderService.getOrderById(id));
+    }
+
+    @GetMapping("/branch/{branchId}")
+    public ResponseEntity<List<OrderDto>> getOrderByBranch(@PathVariable Long branchId , @RequestParam(required = false) Long customerId, @RequestParam(required = false) Long cashierId, @RequestParam(required = false)PaymentType paymentType, @RequestParam(required = false)OrderStatus orderStatus) throws Exception {
+        return  ResponseEntity.ok(orderService.getOrdersByBranch(branchId,customerId,cashierId,paymentType,orderStatus));
+    }
+
+
+
+
 }
