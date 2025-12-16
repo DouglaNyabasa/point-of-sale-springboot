740b315f64ed970f9857462f22233f268683effb
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-16 13:42:50 +0200

    added getallRefunds and createRefund methods in controller

--- src/main/java/com/doug/pointofsale/controller/RefundController.java
+++ src/main/java/com/doug/pointofsale/controller/RefundController.java
@@ -1,4 +1,33 @@
 package com.doug.pointofsale.controller;
 
+import com.doug.pointofsale.models.Refund;
+import com.doug.pointofsale.payload.dto.RefundDTO;
+import com.doug.pointofsale.service.RefundService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/refunds")
 public class RefundController {
+
+    private final RefundService refundService;
+
+    public RefundController(RefundService refundService) {
+        this.refundService = refundService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<RefundDTO> createRefund(@RequestBody RefundDTO refundDTO) throws Exception {
+        RefundDTO refund = refundService.createRefund(refundDTO);
+        return  ResponseEntity.ok(refund);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<RefundDTO>> getAllRefund() throws Exception {
+        List<RefundDTO> refund = refundService.getAllRefund();
+        return  ResponseEntity.ok(refund);
+    }
 }
