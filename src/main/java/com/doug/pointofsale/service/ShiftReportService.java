cbad625578317b1a85ee983486e13ef51491545b
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-17 11:43:12 +0200

    added the ShiftReportService

--- src/main/java/com/doug/pointofsale/service/ShiftReportService.java
+++ src/main/java/com/doug/pointofsale/service/ShiftReportService.java
@@ -1,4 +1,13 @@
 package com.doug.pointofsale.service;
 
+import com.doug.pointofsale.models.ShiftReport;
+import com.doug.pointofsale.payload.dto.ShiftReportDTO;
+
+import java.time.LocalDateTime;
+
 public interface ShiftReportService {
+
+    ShiftReport startShift(Long cashierId, Long branchId, LocalDateTime shiftStart) throws Exception;
+    ShiftReport endShift(Long shiftReportId, LocalDateTime shiftEnd) throws Exception;
+
 }
