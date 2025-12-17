f4070a8af1da2a97b28405ecf6cf6f0ff22b17c5
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-17 11:49:28 +0200

    added the ShiftReportService updated

--- src/main/java/com/doug/pointofsale/service/ShiftReportService.java
+++ src/main/java/com/doug/pointofsale/service/ShiftReportService.java
@@ -1,13 +1,22 @@
 package com.doug.pointofsale.service;
 
+import com.doug.pointofsale.Exception.UserException;
 import com.doug.pointofsale.models.ShiftReport;
 import com.doug.pointofsale.payload.dto.ShiftReportDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ShiftReportService {
 
-    ShiftReport startShift(Long cashierId, Long branchId, LocalDateTime shiftStart) throws Exception;
-    ShiftReport endShift(Long shiftReportId, LocalDateTime shiftEnd) throws Exception;
+    ShiftReportDTO startShift(Long cashierId, Long branchId, LocalDateTime shiftStart) throws Exception;
+    ShiftReportDTO endShift(Long shiftReportId, LocalDateTime shiftEnd) throws Exception;
+    ShiftReportDTO getShiftReportById(Long shiftReportId) ;
+    List<ShiftReportDTO> getAllShiftReports() ;
+    List<ShiftReportDTO> getShiftReportsByBranchId(Long branchId) ;
+    List<ShiftReportDTO> getShiftReportsByCashierId(Long cashierId) ;
+    ShiftReportDTO getCurrentShiftReportsProgress(Long cashierId) throws Exception;
+    ShiftReportDTO getShiftByCashierAndDate(Long cashierId, LocalDateTime date) throws Exception;
+
 
 }
