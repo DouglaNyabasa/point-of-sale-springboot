926a4b01d3766bdb59b8ab3bec2c3045fe63cbd5
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-17 15:05:21 +0200

    added the ShiftReport Repo methods

--- src/main/java/com/doug/pointofsale/repository/ShiftReportRepository.java
+++ src/main/java/com/doug/pointofsale/repository/ShiftReportRepository.java
@@ -1,7 +1,20 @@
 package com.doug.pointofsale.repository;
 
 import com.doug.pointofsale.models.ShiftReport;
+import com.doug.pointofsale.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface ShiftReportRepository extends JpaRepository<ShiftReport, Long> {
+
+    List<ShiftReport> findByCashierId(Long id);
+    List<ShiftReport> findByBranchId(Long id);
+    Optional<ShiftReport> findTopByCashierAndShiftEndIsNullOrderByShiftStartDesc(User cashier);
+
+    Optional<ShiftReport> findTopByCashierAndShiftStartBetween(User cashier, LocalDateTime start, LocalDateTime end);
+    
 }
+
--- src/main/java/com/doug/pointofsale/service/Impl/ShiftReportImpl.java
+++ src/main/java/com/doug/pointofsale/service/Impl/ShiftReportImpl.java
@@ -1,4 +1,60 @@
 package com.doug.pointofsale.service.Impl;
 
-public class ShiftReportImpl {
+import com.doug.pointofsale.models.ShiftReport;
+import com.doug.pointofsale.payload.dto.ShiftReportDTO;
+import com.doug.pointofsale.repository.ShiftReportRepository;
+import com.doug.pointofsale.service.ShiftReportService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ShiftReportImpl implements ShiftReportService {
+
+    private final ShiftReportRepository shiftReportRepository;
+
+    public ShiftReportImpl(ShiftReportRepository shiftReportRepository) {
+        this.shiftReportRepository = shiftReportRepository;
+    }
+
+    @Override
+    public ShiftReportDTO startShift(Long cashierId, Long branchId, LocalDateTime shiftStart) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShiftReportDTO endShift(Long shiftReportId, LocalDateTime shiftEnd) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShiftReportDTO getShiftReportById(Long shiftReportId) {
+        return null;
+    }
+
+    @Override
+    public List<ShiftReportDTO> getAllShiftReports() {
+        return List.of();
+    }
+
+    @Override
+    public List<ShiftReportDTO> getShiftReportsByBranchId(Long branchId) {
+        return List.of();
+    }
+
+    @Override
+    public List<ShiftReportDTO> getShiftReportsByCashierId(Long cashierId) {
+        return List.of();
+    }
+
+    @Override
+    public ShiftReportDTO getCurrentShiftReportsProgress(Long cashierId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShiftReportDTO getShiftByCashierAndDate(Long cashierId, LocalDateTime date) throws Exception {
+        return null;
+    }
 }
