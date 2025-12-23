e5c9b0d5c8d51eb9ffc0015f32eb38f42cf2a1ea
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-23 12:57:17 +0200

    added getshiftbyCashier and getshiftbyBranch methods in shift controller

--- src/main/java/com/doug/pointofsale/controller/ShiftController.java
+++ src/main/java/com/doug/pointofsale/controller/ShiftController.java
@@ -3,11 +3,13 @@ package com.doug.pointofsale.controller;
 
 import com.doug.pointofsale.payload.dto.ShiftReportDTO;
 import com.doug.pointofsale.service.ShiftReportService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/shift-reports")
@@ -33,6 +35,41 @@ public class ShiftController {
                 shiftReportService.endShift(null,null)
         );
     }
+
+    @GetMapping("/current")
+    public ResponseEntity<ShiftReportDTO> getCurrentShiftProgress() throws Exception {
+        return ResponseEntity.ok(
+
+                shiftReportService.getCurrentShiftReportsProgress(null)
+        );
+    }
+
+    @GetMapping("/cashier/{cashierId}/by-date")
+    public ResponseEntity<ShiftReportDTO> getShiftReportByDate(@PathVariable Long cashierId, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime date) throws Exception {
+        return ResponseEntity.ok(
+
+                shiftReportService.getShiftByCashierAndDate(cashierId,date)
+        );
+    }
+
+
+    @GetMapping("/cashier/{cashierId}")
+    public ResponseEntity<List< ShiftReportDTO>> getShiftReportByCashier(@PathVariable Long cashierId) throws Exception {
+        return ResponseEntity.ok(
+
+                shiftReportService.getShiftReportsByCashierId(cashierId)
+        );
+    }
+
+    @GetMapping("/branch/{branchId}")
+    public ResponseEntity<List< ShiftReportDTO>> getShiftReportByBranch(@PathVariable Long branchId) throws Exception {
+        return ResponseEntity.ok(
+
+                shiftReportService.getShiftReportsByBranchId(branchId)
+        );
+    }
+
+
 
 
 
