a1ae41b3122f1f1e02280e10b8dfc8d5f349e9d1
douglanyabasa <douglasnyabasa400@gmail.com>
2026-05-25 17:22:41 +0200

    added payment repository

--- src/main/java/com/doug/pointofsale/repository/PaymentRepository.java
+++ src/main/java/com/doug/pointofsale/repository/PaymentRepository.java
@@ -3,6 +3,7 @@ package com.doug.pointofsale.repository;
 
 import com.doug.pointofsale.models.PaymentEntity;
 import com.doug.pointofsale.models.User;
+import com.doug.pointofsale.payload.response.PaymentStatusDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +17,7 @@ public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
     Optional<PaymentEntity> findFirstByUserAndStatusAndExpiryDateAfter(
             User user, String status, LocalDateTime dateTime);
 
-    @Query("SELECT new com.homequest.dto.response.PaymentStatusDto(p.user.id, p.status, p.amount, p.paidAt, p.expiryDate) " +
+    @Query("SELECT new com.pointofsale.dto.response.PaymentStatusDto(p.user.id, p.status, p.amount, p.paidAt, p.expiryDate) " +
             "FROM PaymentEntity p WHERE p.user.id = :userId ORDER BY p.paidAt DESC LIMIT 1")
     PaymentStatusDto findStatusAndExpiryDateByUserId(@Param("userId") Long userId);
 }
